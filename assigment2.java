efce4a7e5e47d281ccd54a5363d6860b071ef8c5
Koyiljon448 <144305972+Koyiljon448@users.noreply.github.com>
2024-03-20 21:16:04 +0900

    Assignment2-week2

---
+++ assigment2.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class assigment2{
+    public static void main(String[] args){
+      LocalTime now = LocalTime.now();
+      System.out.println(now);  
+    }
+} 
